c9786429ba9e843f4629e6520dfda01d3bde9fd7
Hunaid2000 <hunaid2000@gmail.com>
2022-03-25 18:29:39 +0500

    Register Store, Add Products, Image Scrolling features implemented. UI tweaks + UI bug fixes. Code Refactored

--- supermartApp/models.py
+++ supermartApp/models.py
@@ -1,8 +1,28 @@
 from django.db import models
-from django import forms
 
 class Account(models.Model):
     name = models.CharField(max_length=50)
     email = models.EmailField(unique=True)
     password = models.CharField(max_length=20)
-    is_seller= models.BooleanField(default=False)
+    is_seller = models.BooleanField(default=False)
+
+class Store(models.Model):
+    store_name = models.CharField(max_length=50, unique=True)
+    contact = models.CharField(max_length=20)
+    location = models.TextField()
+    seller = models.ForeignKey(Account, on_delete=models.CASCADE)
+
+class Product(models.Model):
+    name = models.CharField(max_length=50)
+    brand = models.CharField(max_length=50)
+    price = models.FloatField()
+    total_stock = models.IntegerField()
+    small = models.IntegerField(null=True, blank=True, default=None)
+    medium = models.IntegerField(null=True, blank=True, default=None)
+    large = models.IntegerField(null=True, blank=True, default=None)
+    description = models.TextField()
+    store = models.ForeignKey(Store, on_delete=models.CASCADE)
+
+class ProductImages(models.Model):
+    image = models.ImageField(upload_to="product_images/")
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
--- supermartApp/views.py
+++ supermartApp/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
 from django.contrib import messages
-from supermartApp.models import Account
+from supermartApp.models import Account, Product, ProductImages, Store
 
 
 def home(request):
-    return render(request, 'home.html')
+    if 'user_id' not in request.session:
+        images = ProductImages.objects.all()
+    elif 'is_seller' in request.session and request.session['is_seller'] == 1:
+        images = ProductImages.objects.filter(product__store__seller_id=request.session['user_id'])  
+    return render(request, 'home.html', {'images':images})
 
 def logout(request):
     del request.session['user_id']
@@ -29,11 +32,51 @@ def accounts(request):
         if Account.objects.filter(email=email, password=password).exists():
             user = Account.objects.get(email=email, password=password)
             request.session['user_id'] = user.id
+            request.session['is_seller'] = user.is_seller
             return redirect('home')
         else:
             messages.error(request, 'Incorrect Email/Password')    
     return render(request, 'accounts.html')
+
 def registerstore(request):
+    if request.method == 'POST':
+        store_name = request.POST['store_name']
+        contact = request.POST['contact']
+        location = request.POST['location']
+        seller = Account.objects.get(pk=request.session['user_id'])
+        if Store.objects.filter(store_name=store_name).exists():
+            messages.error(request, 'This Store name is already in use')
+            return redirect('registerstore')
+        store = Store(store_name=store_name, contact=contact, location=location, seller=seller)
+        store.save()  
     return render(request, 'registerstore.html')
+
 def addproduct(request):
-    return render(request, 'addproduct.html')
+    if request.method == 'POST':
+        name = request.POST['name']
+        brand = request.POST['brand']
+        price = request.POST['price']
+        total_stock = request.POST['total_stock']
+        small = request.POST['small']
+        if small == '':
+            small = 0
+        small = int(small)            
+        medium = request.POST['medium']
+        if medium == '':
+            medium = 0
+        medium = int(medium)
+        large = request.POST['large']
+        if large == '':
+            large = 0
+        large = int(large)
+        description = request.POST['description']
+        images = request.FILES.getlist('images')
+        store = Store.objects.get(pk=request.POST['storename'])        
+        product = Product(name=name, brand=brand, price=price, total_stock=total_stock, small=small, medium=medium, large=large, description=description, store=store)
+        product.save()  
+        for img in images:
+            productImage = ProductImages(image=img, product=product)
+            print(productImage)
+            productImage.save()
+    stores = Store.objects.filter(seller_id=request.session['user_id'])
+    return render(request, 'addproduct.html', {'stores':stores})
